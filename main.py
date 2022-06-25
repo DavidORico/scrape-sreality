3d5670c68d085875863fa8ac86cce5b67f3e1d94
David O. Rico <david.rico@datavision.cz>
2022-06-25 11:40:39 +0200

    Fix docker launch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scrapyscript import Job, Processor
-import json
+import logging
 from spider.spiders.sreality_spider import SrealitySpider
 from src.database_estates import DatabaseEstates
 from http.server import HTTPServer
@@ -8,6 +8,7 @@ from src.web_server import MyServer
 
 def main():
     # start web scraping job of sreality and get yielded data
+    logging.info('Starting web scraping job.')
     srealityJob = Job(SrealitySpider)
 
     processor = Processor(settings=None)
@@ -15,12 +16,14 @@ def main():
     #print(json.dumps(data, indent=4))
 
     # initialize and add data to the database
+    logging.info('Establishing connection to database.')
     db_estates = DatabaseEstates()
     db_estates.insert(data)
     #db_estates.view_all()
 
     # run web server that displays results on http://127.0.0.1:8080/show
-    hostName = "127.0.0.1"
+    logging.info('Creating python web server.')
+    hostName = "0.0.0.0"
     serverPort = 8080
     myserver = MyServer
     myserver.data = db_estates.get_all()
